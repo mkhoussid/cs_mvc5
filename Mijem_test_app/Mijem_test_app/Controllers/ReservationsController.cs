636d87e38784cc0dcb2fe26393e7fb985bf754d4
Unknown <khmike@hotmail.ca>
2018-10-22 14:49:53 -0400

    Enabled migrations

--- Mijem_test_app/Mijem_test_app/Controllers/ReservationsController.cs
+++ Mijem_test_app/Mijem_test_app/Controllers/ReservationsController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Mijem_test_app.Models;
 
 namespace Mijem_test_app.Controllers
 {
     public class ReservationsController : Controller
     {
+        //instantiate db cursor
+        private ApplicationDbContext _context;
         // GET: Reservations
         public ActionResult Index()
         {
