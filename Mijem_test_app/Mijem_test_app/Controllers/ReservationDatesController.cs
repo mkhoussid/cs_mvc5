fd84cd07fdba3a0ea3029fbfea5cc5c4768ba9bc
Unknown <khmike@hotmail.ca>
2018-10-24 19:11:48 -0400

    Build on Update Reservation

--- Mijem_test_app/Mijem_test_app/Controllers/ReservationDatesController.cs
+++ Mijem_test_app/Mijem_test_app/Controllers/ReservationDatesController.cs
@@ -59,7 +59,16 @@ namespace Mijem_test_app.Controllers
             return View(locations);
         }
 
-        public ActionResult Save(ReservationDate reservation)
+        public ActionResult Save(Reservation reservation)
+        {
+            _context.Reservations.Add(reservation);
+
+            _context.SaveChanges();
+
+            return View("Index");
+        }
+
+        public ActionResult Update(ReservationDate reservation)
         {
             var _reservation = _context.ReservationDates
                 .SingleOrDefault(r => r.Id == reservation.Id);
@@ -67,15 +76,12 @@ namespace Mijem_test_app.Controllers
             _reservation.InfoFromTextBox = reservation.InfoFromTextBox;
             _context.SaveChanges();
             return RedirectToAction("Index");
-        }
 
-        public ActionResult Update(ReservationDate reservation)
-        {
-            var _reservation = _context.ReservationDates
-                .Include(r => r.Reservation)
-                .Include(r => r.Contact)
-                .SingleOrDefault(r => r.Id == reservation.Id);
-            return Content("Passed");
+            //var _reservation = _context.ReservationDates
+            //    .Include(r => r.Reservation)
+            //    .Include(r => r.Contact)
+            //    .SingleOrDefault(r => r.Id == reservation.Id);
+            //return Content("Passed");
         }
 
         [HttpPost]
